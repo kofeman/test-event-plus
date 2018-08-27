c771e679c494ab9100ae1c02ea806265d6fc0b58
Nikolay Starikov <nickolay-starikov@ya.ru>
2018-08-27 19:39:37 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	fileinclude = require('gulp-file-include'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload;
+
+// html
+
+gulp.task('html', function() {
+	return gulp.src(['html/*.html'])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file',
+			indent: true
+		}))
+		.on('error', console.log)
+		.pipe(gulp.dest('.'));
+});
+
+//less
+
+gulp.task('scss', function() {
+	return gulp.src(['css/style.scss'])
+		.pipe(sass())
+		.on('error', console.log)
+		.pipe(autoprefixer({
+
+		}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('scss-bootstrap', function() {
+	return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss'])
+		.pipe(sass())
+		.on('error', console.log)
+		.pipe(autoprefixer({
+		
+		}))
+		.pipe(gulp.dest('css'));
+});
+
+gulp.task('reload', function() {
+	browserSync.reload();
+	return true;
+});
+
+
+//default task
+
+gulp.task('default', function() {
+
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+	gulp.watch('css/**', ['scss', 'reload']);
+	gulp.watch('html/**', ['html', 'reload']);
+	gulp.watch('js/**', ['reload']);
+	gulp.watch('images/**', ['reload']);
+});
---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function ($) {
+
+// CODE STYLE //
+	
+	/* FUNCTION NAME */
+
+// (function () {
+// 	var $element = $(".element");
+//
+// 	if ($element.length) {
+//
+// 		$element.on("click", function () {
+// 			someFunction()
+// 		});
+//
+// 		function someFunction() {
+// 			// DO SOMETHING
+// 		}
+// 	}
+// })();
+	
+	/* END FUNCTION */
+	
+});
