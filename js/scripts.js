cd42237442b807945101702a1e21e560c86d0c0c
Nikolay Starikov <nickolay-starikov@ya.ru>
2018-09-05 14:43:48 +0300

    add drag-and-drop and adding inputs

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,73 @@ $(document).ready(function ($) {
 	
 	/* END FUNCTION */
 	
+	/* DRAG AND DROP */
+	
+	$( "#sortable" ).sortable();
+	//$( "#sortable" ).disableSelection();
+	
+	/* END DRAG AND DROP */
+	
+	
+	$('.js-add-time').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-time').hide();
+		
+		$('.js-time').show();
+	});
+	
+	// $('.js-add-place').on('click', function (e) {
+	// 	e.preventDefault();
+	//
+	// 	$('.js-place').show();
+	// })
+	
+	$('.js-add-date').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-date').hide();
+		
+		$('.js-date').show();
+	})
+	
+	$('.js-add-input-yt').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-input-yt').addClass('active');
+		
+		$('.social__input_yt').show();
+	})
+	
+	$('.js-add-input-vk').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-input-vk').addClass('active');
+		
+		$('.social__input_vk').show();
+	})
+	
+	$('.js-add-input-inst').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-input-inst').addClass('active');
+		
+		$('.social__input_inst').show();
+	})
+	
+	$('.js-add-input-sound').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-input-sound').addClass('active');
+		
+		$('.social__input_sound').show();
+	})
+	
+	$('.js-add-input-home').on('click', function (e) {
+		e.preventDefault();
+		
+		$('.js-add-input-home').addClass('active');
+		
+		$('.social__input_home').show();
+	})
 });
